fb4db1abc5cdbc078f36fe11b5fb766b502d5a75
AlexStorzh <ktgcov57@gmail.com>
2023-01-24 12:53:11 +0200

    Styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className={style.footer}>
       <div>@2023 - Ukraine</div>
-      <div>HUNTNTIE&reg;</div>
+      <div>HUNTNITE&reg;</div>
       <div>All right reserved</div>
     </footer>
   );
